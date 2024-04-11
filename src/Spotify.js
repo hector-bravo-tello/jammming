f5ade629b8fd342f3f1b13b037e7e519a9ffaddf
Hector Bravo <hgbravo@gmail.com>
2024-04-11 14:03:00 -0500

    final commit

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,6 +1,8 @@
 // Spotify.js
 const clientId = 'c631296f40df4f59821d59bdb4e27095'; // Replace with your Spotify client ID
-const redirectUri = 'http://localhost:3000/'; // Your redirect URI
+//const redirectUri = 'http://localhost:3000/'; // Your development redirect URI
+const redirectUri = 'https://jammming-hb.netlify.app/'; // Your redirect URI
+
 let accessToken;
 
 const Spotify = {
